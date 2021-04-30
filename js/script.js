e62c68f557c9cffed0f10042822782ccb7ae7603
Iriuna <irenserge@mail.ru>
2021-04-30 15:16:27 +0300

    all sections+prepros

--- js/script.js
+++ js/script.js
@@ -5,7 +5,57 @@ $(document).ready(function(){
         infinite: true,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        responsive: [
+           {
+            breakpoint: 1024,
+            settings: {
+              infinite: true,
+              dots: false, 
+            }
+          },
+        ],
     });
     
 });
+
+$(document).ready(function(){
+    $('.works-slider').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+               responsive: [
+            {
+                breakpoint: 2048,
+                settings: "unslick",
+            },
+            {
+                breakpoint: 1100,
+                settings: { 
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false, 
+                }
+              },
+            {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: false, 
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ],
+      });
+})
